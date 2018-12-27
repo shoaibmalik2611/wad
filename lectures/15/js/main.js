4f1795a7262063e676d08dae111034e39056307c
Muhamad Shoaib MAlik <shabi@ucp.edu.pk>
2018-12-27 17:27:04 +0500

    lalalala

--- lectures/15/js/main.js
+++ lectures/15/js/main.js
@@ -7,13 +7,10 @@ var Account= {
 
 var month = ["January", "Febrary", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
 
-document.getElementById("title").innerHTML=Account.title;
-document.getElementById("currency").innerHTML=Account.currency;
-document.getElementById("balance").innerHTML=Account.balance;
-document.getElementById("IBAN").innerHTML=Account.IBAN;
+
 
 function deposit() {
-    var d = document.getElementById("Deposit Amount");
+
     if(event.keyCode===13){
         Account.balance = Account.balance + parseInt(document.getElementById("deposit").value);
         document.getElementById("balance").innerHTML=Account.balance;
